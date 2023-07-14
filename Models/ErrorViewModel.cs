4e96ce4c8d35320bfbddd0e3d8508e2e261a60c0
luan-natan <sousaluan.natan01@gmail.com>
2023-07-13 22:11:39 -0300

    Primeiro commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WishLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
