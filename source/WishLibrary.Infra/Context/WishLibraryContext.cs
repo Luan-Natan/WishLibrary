a5b8adfa64194b4f3a79429a782d7a58e865a7cf
luan-natan <sousaluan.natan01@gmail.com>
2023-07-19 18:46:41 -0300

    Configurando mapeamento das tabelas

---
+++ source/WishLibrary.Infra/Context/WishLibraryContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using WishLibrary.Core.Models;
+
+namespace WishLibrary.Infra.Context
+{
+    public partial class WishLibraryContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public WishLibraryContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public WishLibraryContext(DbContextOptions<WishLibraryContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_configuration["ConnectionString"]);
+            }
+        }
+
+        public virtual DbSet<Livro> Livro { get; set; }
+        public virtual DbSet<Genero> Genero { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(WishLibraryContext).Assembly);
+        }
+    }
+}
---
+++ source/WishLibrary.Infra/Migrations/20230719210659_initialMigration.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WishLibrary.Infra.Migrations
+{
+    public partial class initialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "T_GENERO",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "VARCHAR(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_GENERO", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "T_LIVRO",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    AnoLancamento = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    GeneroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_T_LIVRO", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_T_LIVRO_T_GENERO_GeneroId",
+                        column: x => x.GeneroId,
+                        principalTable: "T_GENERO",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_T_LIVRO_GeneroId",
+                table: "T_LIVRO",
+                column: "GeneroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "T_LIVRO");
+
+            migrationBuilder.DropTable(
+                name: "T_GENERO");
+        }
+    }
+}
